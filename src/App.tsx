b0d9e7a45700716d4ef33ee419213b9546622aec
andrew1994sanjose1 <andrew1994sanjose@gmail.com>
2026-04-27 10:24:42 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/common/Navbar';
-import { Footer } from './components/common/Footer';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { Trial } from './pages/Trial';
 import { MemberPortal } from './pages/MemberPortal';
